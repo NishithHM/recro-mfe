9098154252208811fe02a87f91042af1b033053e
NishithHM <nishithhm@Apples-MacBook-Pro.local>
2021-09-15 15:02:17 +0530

    added some landing page

---
+++ recro-product/src/landingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const LandingPage = ()=>{
+    console.log('landing page')
+
+    return(
+        <div>
+            <div>
+            Landing page
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
